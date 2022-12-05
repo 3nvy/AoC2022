efef35c6d72bf14b05ec4221402aa9f61552242f
Renato Pestana <renatompestana@gmail.com>
2022-12-05 00:10:17 +0000

    Add day4

---
+++ day_4/part2.mjs
@@ -0,0 +1,13 @@
+import { readInput } from "../utils.mjs";
+
+const input = readInput({
+  filePath: "day_4/input.txt",
+  parseFn: (input) => input.map(row => row.split(/[-:,]/).map(val => +val)),
+});
+
+const result = input.reduce((acc, s) => {
+    if(!(s[1] < s[2] || s[3] < s[0])) acc++
+    return acc;
+} , 0)
+
+console.log(result)
