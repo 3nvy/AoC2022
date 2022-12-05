240e8333e8af9783e83d2cb555d07458682c6873
Renato Pestana <renatompestana@gmail.com>
2022-12-05 00:12:29 +0000

    Remove unecessary character from day4 regex input parse

--- day_4/part2.mjs
+++ day_4/part2.mjs
@@ -2,7 +2,7 @@ import { readInput } from "../utils.mjs";
 
 const input = readInput({
   filePath: "day_4/input.txt",
-  parseFn: (input) => input.map(row => row.split(/[-:,]/).map(val => +val)),
+  parseFn: (input) => input.map(row => row.split(/[-,]/).map(val => +val)),
 });
 
 const result = input.reduce((acc, s) => {
